81efeff59c12380114debebcbfd708f31175f6fd
Zain Afzal <zain.afz@gmail.com>
2018-12-02 22:42:30 +1100

    bot!

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 import discord
 import os
+import re
 from loggers import hodge_logger
+from modules.Core import Core
 
 client = discord.Client()
+call_chain = [Core()]
 
+def preprocess(s):
+    s = re.sub("<@!431280056468242435>","!hp",s)
+    s = re.sub("hodge\s+podge","!hp",s,flags=re.IGNORECASE)
+    return s
 
 @client.event
 async def on_ready():
@@ -15,7 +22,10 @@ async def on_message(message):
     # ignore bots
     if (message.author.bot):
         return
-
+    # preprocess
+    m = preprocess(message.content)
+    for module in call_chain:
+        await module.message(m)
 
 def run():
     hodge_logger.info("Launching Bot")
---
+++ modules/Core.py
@@ -0,0 +1,11 @@
+from util.Response import Response
+from util.matching import trigger
+
+class Core:
+    def __init__(self):
+        self.doc_link = "http://zainafzal08.github.io/HodgePodge"
+
+    async def message(m):
+        if trigger(m,"help","show documentation","docs","how do i.*"):
+            msg = "I have a nice list of what i can do at {}".format(self.doc_link)
+            return Response(msg)
